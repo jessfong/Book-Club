43795d768823b99af3a955b4da9df2b95fe7e810
jessfong <jessicafong323@gmail.com>
2019-07-13 20:22:21 -0400

    First commit

---
+++ BookClubServer/BookClubServer/Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookClubServer.Data
+{
+    public class User
+    {
+        public string ID { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ BookClubServer/BookClubServer/Models/UserCreateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookClubServer.Models
+{
+    public class UserCreateModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
