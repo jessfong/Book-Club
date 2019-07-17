b3d6fdd60bc7680a08a61a312cac71bc23fc6c2a
jessfong <jessicafong323@gmail.com>
2019-07-17 18:01:35 -0400

    Created Database
    Created empty database for book club

---
+++ BookClubServer/BookClubServer/Data/BookClubContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookClubServer.Data
+{
+    public class BookClubContext : DbContext
+    {
+        public BookClubContext(DbContextOptions<BookClubContext> options)
+            : base(options)
+        {
+        } 
+
+
+    }
+}
