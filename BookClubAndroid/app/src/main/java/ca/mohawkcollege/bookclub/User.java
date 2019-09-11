4c1b853f84c948aaf314712f139528b3b20b5084
jessf1 <jessicafong323@gmail.com>
2019-09-11 16:45:58 -0400

    Added layout pages

---
+++ BookClubAndroid/app/src/main/java/ca/mohawkcollege/bookclub/User.java
@@ -0,0 +1,13 @@
+package ca.mohawkcollege.bookclub;
+
+public class User {
+    public String Email;
+    public String Password;
+
+    public User(){}
+
+    public User(String email, String password){
+        this.Email = email;
+        this.Password = password;
+    }
+}
