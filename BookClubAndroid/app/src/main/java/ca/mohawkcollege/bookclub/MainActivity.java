7dd66aca95c6591f498681c2bdb3badc4d83b230
jessf1 <jessicafong323@gmail.com>
2019-09-12 12:28:07 -0400

    Added signIn layout

--- BookClubAndroid/app/src/main/java/ca/mohawkcollege/bookclub/MainActivity.java
+++ BookClubAndroid/app/src/main/java/ca/mohawkcollege/bookclub/MainActivity.java
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
